8db71abe0847dacdc7ddeccf1c4284788a4bc834
Agnieszka Kowalik <35640119+agnkow@users.noreply.github.com>
2019-12-20 17:01:45 +0100

    Add files via upload

---
+++ img_url.py
@@ -0,0 +1,11 @@
+# img_url
+
+img_gas = "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/79456351_113133966850599_1386038495344066560_n.png?_nc_cat=106&_nc_oc=AQmf7SnyVvafMWT1p7D_Y_YXcchxkfKe13hOl3g-OhgpF0D24PWH8NCeEtSi_mqMh0E&_nc_ht=scontent-waw1-1.xx&oh=7d77040d67d40ae2bd9d694877f0ab68&oe=5EB12107"
+
+img_energy = "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/79883389_113133953517267_746628567751720960_n.png?_nc_cat=101&_nc_oc=AQmWVEVLqkBseWSWz9uDosSC_CjhHtbhJPbZIvnuMrQGb9lECyliMDqXpD36_UwXy1M&_nc_ht=scontent-waw1-1.xx&oh=72be230065b1e8ce34351d5be55a6ab8&oe=5E772489"
+
+img_property_rights = "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/81161302_113171033513559_7451161624494211072_n.png?_nc_cat=109&_nc_oc=AQkkQZ_tDsYAmss0bVDVPKL5p5n5pWfisiNsXzaNb2AAxPNItZpevDOjIrAiV20kCDk&_nc_ht=scontent-waw1-1.xx&oh=073a37b7cdca330d82338702aae1dd85&oe=5E6AFFBA"
+
+img_coal = "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/79682252_113160980181231_2183753955324985344_n.png?_nc_cat=109&_nc_oc=AQm8Dl8aWlRKUTp5KAZSSpPvddi7251lXuwyUcewsHmO-AnigQVq1gyLiDlql9bKFv8&_nc_ht=scontent-waw1-1.xx&oh=d06a602c1dc16bfe65615d1a61941f11&oe=5EACE797"
+
+img_wind = "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/79514826_113161186847877_6763138727310000128_n.png?_nc_cat=104&_nc_oc=AQnSv2mvt0xftx1TUeW4j2y_VVcEt2gJgQX5DDrgOHWnvPU4wt1F6TkEMJbpOJa_8QY&_nc_ht=scontent-waw1-1.xx&oh=65f4dd27da6c9c9440dd7ea0898259e5&oe=5E7BD9CA"
